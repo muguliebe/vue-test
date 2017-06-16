2f705bb5c487719ff993f6a354ae9ec570a48718
zany <muguliebe@gmail.com>
2017-06-16 12:54:52 +0900

    ing

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    I'm Test and.. {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'shit'
+    }
+  }
+}
+</script>
